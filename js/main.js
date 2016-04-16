299d7e503a4854dad9296d6860ca80bab7ac90bd
Samantha Webb <samanthawebb@outlook.com>
2016-04-15 20:54:35 -0400

    add box-sizing to main.css

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ $(document).ready(function() {
        animationTime: 1000,             // AnimationTime let you define how long each section takes to animate
        pagination: false,                // You can either show or hide the pagination. Toggle true for show, false for hide.
        updateURL: false,                // Toggle this true if you want the URL to be updated automatically when the user scroll to each page.
+       loop: false,                     // You can have the page loop back to the top/bottom when the user navigates at up/down on the first/last page.
        keyboard: true,                  // You can activate the keyboard controls
        responsiveFallback: false,        // You can fallback to normal page scroll by defining the width of the browser in which
                                         // you want the responsive fallback to be triggered. For example, set this to 600 and whenever
