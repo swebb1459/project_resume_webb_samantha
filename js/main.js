b8d47a49a6ade4c5371ff68524d7c318e3b5997b
Samantha Webb <samanthawebb@outlook.com>
2016-04-16 21:36:16 -0400

    define cursor for hover for #more_buttom

--- js/main.js
+++ js/main.js
@@ -2,4 +2,8 @@ $(document).ready(function() {
     $.backstretch("img/img1.jpg");
 
     $('.focuspoint').focusPoint();
+
+    $('#more_button').click(function(){
+        $('#testimonial').text('sdfadsfasddsads');
+    });
 });
